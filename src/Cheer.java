baf44bf2e022db04834e1bfc68357ca817c9db29
Eszter Viczian <eviczia@gmail.com>
2020-04-29 11:25:58 +0200

    d6

--- src/Cheer.java
+++ src/Cheer.java
@@ -1,6 +1,6 @@
 public class Cheer {
 
-    public static void Cheer() {
-        System.out.println("Java");
+    public static void Cheer(String param) {
+        System.out.println("Cheer for param " + param);
     }
 }
